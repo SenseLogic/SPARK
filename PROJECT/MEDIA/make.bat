c49e0e1613c4e91c1db520978d52259bc72c9c99
senselogic <dev.senselogic@gmail.com>
2021-02-19 21:57:43 +0100

    % block, % font, % media

--- PROJECT/MEDIA/make.bat
+++ PROJECT/MEDIA/make.bat
@@ -1,2 +1,3 @@
 for %%f in (static\image\xxx\*.jpg) do ..\..\..\..\TOOL\IMAGE_MAGICK\convert %%f -quality 90 ..\www\static\image\xxx\%%~nf.jpg
+..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 32x32 -colors 256 ..\www\favicon.ico
 pause
