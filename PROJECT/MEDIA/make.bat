fd3f04c0472a80bb1112aaf323dd1f7d10ab8b6d
senselogic <dev.senselogic@gmail.com>
2021-02-22 21:12:26 +0100

    % favicon

--- PROJECT/MEDIA/make.bat
+++ PROJECT/MEDIA/make.bat
@@ -1,3 +1,2 @@
 for %%f in (upload\image\*.jpg) do ..\..\..\..\TOOL\IMAGE_MAGICK\convert %%f -quality 90 ..\www\upload\image\%%~nf.jpg
-..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 32x32 -colors 256 ..\www\favicon.ico
 pause
--- PROJECT/MEDIA/make.sh
+++ PROJECT/MEDIA/make.sh
@@ -3,4 +3,3 @@ set -x
 for f in upload/image/*.jpg; do
     convert $f -quality 90 ../www/$f
 done
-convert favicon.png -resize 32x32 -colors 256 ../www/favicon.ico
