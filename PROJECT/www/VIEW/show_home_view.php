d59c5d337684e1cf112cd44e847afeabf3a85298
senselogic <dev.senselogic@gmail.com>
2022-03-17 21:02:20 +0100

    % styling

--- PROJECT/www/VIEW/show_home_view.php
+++ PROJECT/www/VIEW/show_home_view.php
@@ -1,6 +1,6 @@
 
 
-<div class="extended-container view" data-view-name="home">
+<div class="is-hidden extended-container view" data-view-name="home">
     <?php require __DIR__ . '/' . 'BLOCK/article_carousel.php'; ?>
     <?php require __DIR__ . '/' . 'BLOCK/article_list.php'; ?>
 </div>
