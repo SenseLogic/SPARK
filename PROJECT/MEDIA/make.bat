8cf9f210831510f761858f450bbb5f2f16806ff2
senselogic <dev.senselogic@gmail.com>
2022-03-21 22:58:27 +0100

    % media

--- PROJECT/MEDIA/make.bat
+++ PROJECT/MEDIA/make.bat
@@ -1,2 +1,2 @@
-for %%f in (static\image\test\*.jpg) do ..\..\..\..\TOOL\IMAGE_MAGICK\convert %%f -quality 90 ..\www\static\image\test\%%~nf.jpg
+for %%f in (static\image\test\*.*) do ..\..\..\..\TOOL\IMAGE_MAGICK\convert %%f -quality 90 ..\www\static\image\test\%%~nf.jpg
 pause
--- PROJECT/MEDIA/make.sh
+++ PROJECT/MEDIA/make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -x
-for f in static/image/test/*.jpg; do
+for f in static/image/test/*.*; do
     convert $f -quality 90 ../www/$f
 done
