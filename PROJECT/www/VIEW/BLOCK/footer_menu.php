2ff593ffe5d98a2be1af9c44625ac72278bc882c
senselogic <dev.senselogic@gmail.com>
2022-03-22 20:29:09 +0100

    % admin, % footer, % text

--- PROJECT/www/VIEW/BLOCK/footer_menu.php
+++ PROJECT/www/VIEW/BLOCK/footer_menu.php
@@ -27,16 +27,16 @@
 
 
 <div class="large-padded-container footer-menu">
-    <span class="margin-right-auto! scaled-button footer-menu-button" onclick="ShowView( 'home' )">
+    <div class="margin-right-auto! scaled-button footer-menu-button" onclick="ShowView( 'home' )">
         <?php echo $this->GetText( 'FooterMenuHomeButton' ); ?>
-    </span>
-    <span class="scaled-button footer-menu-button" onclick="ShowView( 'home/#article-list' )">
+    </div>
+    <div class="scaled-button footer-menu-button" onclick="ShowView( 'home/#article-list' )">
         <?php echo $this->GetText( 'FooterMenuArticleListButton' ); ?>
-    </span>
-    <span class="scaled-button footer-menu-button" onclick="ShowView( 'contact' )">
+    </div>
+    <div class="scaled-button footer-menu-button" onclick="ShowView( 'contact' )">
         <?php echo $this->GetText( 'FooterMenuContactButton' ); ?>
-    </span>
-    <span class="margin-left-auto! scaled-button footer-menu-button" onclick="ShowView( 'legal-notice' )">
+    </div>
+    <div class="margin-left-auto! scaled-button footer-menu-button" onclick="ShowView( 'legal-notice' )">
         <?php echo $this->GetText( 'FooterMenuLegalNoticeButton' ); ?>
-    </span>
+    </div>
 </div>
