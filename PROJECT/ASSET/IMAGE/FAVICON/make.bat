1febd7df7f06818928fa33f4f6cf0298757d4416
senselogic <dev.senselogic@gmail.com>
2022-06-04 09:41:02 +0200

    % images

--- PROJECT/ASSET/IMAGE/FAVICON/make.bat
+++ PROJECT/ASSET/IMAGE/FAVICON/make.bat
@@ -1,11 +1,11 @@
 copy favicon.svg ..\..\..\www\favicon.svg
-..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -background none -resize 48x48 -density 48x48 ../../../www/favicon.ico
-..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 48x48 ../../../www/favicon-48x48.png
-..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 96x196 ../../../www/favicon-96x96.png
-..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 144x144 ../../../www/favicon-144x144.png
-..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 192x192 ../../../www/favicon-180x180.png
-..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 192x192 ../../../www/favicon-192x192.png
-..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 256x256 ../../../www/favicon-256x256.png
-..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 384x384 ../../../www/favicon-384x384.png
-..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 512x512 ../../../www/favicon-512x512.png
+..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -background none -resize 48x48 -density 48x48 -strip ../../../www/favicon.ico
+..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 48x48 -strip ../../../www/favicon-48x48.png
+..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 96x196 -strip ../../../www/favicon-96x96.png
+..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 144x144 -strip ../../../www/favicon-144x144.png
+..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 192x192 -strip ../../../www/favicon-180x180.png
+..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 192x192 -strip ../../../www/favicon-192x192.png
+..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 256x256 -strip ../../../www/favicon-256x256.png
+..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 384x384 -strip ../../../www/favicon-384x384.png
+..\..\..\..\..\..\TOOL\IMAGE_MAGICK\convert favicon.png -resize 512x512 -strip ../../../www/favicon-512x512.png
 pause
--- PROJECT/MEDIA/make.sh
+++ PROJECT/MEDIA/make.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -x
 for f in static/image/article/*.*; do
-    convert $f -resize 2073600@ -quality 70 ../www/$f
+    convert $f -resize 2073600@ -quality 70 -strip ../www/$f
 done
 for f in static/image/contact/*.*; do
-    convert $f -resize 2073600@ -quality 70 ../www/$f
+    convert $f -resize 2073600@ -quality 70 -strip ../www/$f
 done
 for f in static/image/article/*.*; do
-    convert $f -resize 147456@ -quality 50 ../www/$f.preload.jpg
+    convert $f -resize 147456@ -quality 50 -strip ../www/$f.preload.jpg
 done
 for f in static/image/contact/*.*; do
-    convert $f -resize 147456@ -quality 50 ../www/$f.preload.jpg
+    convert $f -resize 147456@ -quality 50 -strip ../www/$f.preload.jpg
 done
