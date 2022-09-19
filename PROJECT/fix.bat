0230558affeed1bd434ebe7617bf1ce9a3347958
senselogic <dev.senselogic@gmail.com>
2022-09-19 19:23:55 +0100

    % fix

--- PROJECT/fix.bat
+++ PROJECT/fix.bat
@@ -1,2 +1,5 @@
 ..\..\..\TOOL\FLEX\flex fix.flex
+rename MEDIA\static\image\article MEDIA\static\image\article
+rename www\static\image\article www\static\image\article
+rename www\static\video\article www\static\video\article
 pause
