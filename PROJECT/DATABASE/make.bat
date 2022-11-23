9ed4b2298bd900722c61e48e26c4b07a63e2c825
senselogic <dev.senselogic@gmail.com>
2022-11-23 08:04:54 +0100

    + include

--- PROJECT/DATABASE/make.bat
+++ PROJECT/DATABASE/make.bat
@@ -1,8 +1,8 @@
-..\..\..\..\TOOL\BASIL\basil --sql --exclude-command "drop schema" --exclude-command "create schema" spark_project.bs text.bd language.bd page_type.bd home_page.bd article_page.bd article_list_page.bd cookie_policy_page.bd legal_notice_page.bd contact_page.bd block_category.bd block_type.bd text_block.bd image_block.bd text_and_image_block.bd user.bd
+..\..\..\..\TOOL\BASIL\basil --sql --exclude-command "drop schema" --exclude-command "create schema" spark_project.bs spark_project.bd
 copy spark_project.sql remote_spark_project.sql
 copy spark_project_data.sql remote_spark_project_data.sql
 copy spark_project_dump.sql remote_spark_project_dump.sql
-..\..\..\..\TOOL\BASIL\basil --sql --phoenix --force-drop spark_project.bs text.bd language.bd page_type.bd home_page.bd article_page.bd article_list_page.bd cookie_policy_page.bd legal_notice_page.bd contact_page.bd block_category.bd block_type.bd text_block.bd image_block.bd text_and_image_block.bd user.bd spark_project.bt administration.bt
+..\..\..\..\TOOL\BASIL\basil --sql --phoenix --force-drop spark_project.bs spark_project.bd spark_project.bt administration.bt
 pause
 ..\..\..\..\TOOL\CYCLONE\cyclone mysql 127.0.0.1 3306 root "" spark_project.sql spark_project_data.sql
 copy PHX\index.phx ..\CODE\
