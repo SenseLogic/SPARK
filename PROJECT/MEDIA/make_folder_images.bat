26c6913b503d0eab7b8e5e682f8ffd793bcae6d3
senselogic <dev.senselogic@gmail.com>
2022-11-28 21:44:15 +0100

    % style, % media

--- PROJECT/MEDIA/make_folder_images.bat
+++ PROJECT/MEDIA/make_folder_images.bat
@@ -1,3 +1,4 @@
+if not exist "..\www\static\image\%1" mkdir "..\www\static\image\%1"
 for %%f in (static\image\%1\*.jpg) do ..\..\..\..\TOOL\IMAGE_MAGICK\convert "%%f" -resize 2073600@ -quality 70 -strip "..\www\static\image\%1\%%~nf.jpg"
 for %%f in (static\image\%1\*.png) do ..\..\..\..\TOOL\IMAGE_MAGICK\convert "%%f" -background white -alpha remove -alpha off -resize 2073600@ -quality 70 -strip "..\www\static\image\%1\%%~nf.jpg"
 for %%f in (static\image\%1\*.jpg) do ..\..\..\..\TOOL\IMAGE_MAGICK\convert "%%f" -resize 147456@ -quality 50 -strip "..\www\static\image\%1\%%~nf.jpg.preload.jpg"
