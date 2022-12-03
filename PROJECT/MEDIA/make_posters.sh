5fd100249f1e00c83850400f6c0c868def64a799
senselogic <dev.senselogic@gmail.com>
2022-12-03 12:13:23 +0100

    % media

--- PROJECT/MEDIA/make_posters.sh
+++ PROJECT/MEDIA/make_posters.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -x
 for f in static/video/article/*.*; do
-    ffmpeg -y -i $f -frames:v 1 -strip static/image/article/${f%.*}_video_background.png
+    ffmpeg -y -i $f -frames:v 1 static/image/article/${f%.*}_video_background.png
 done
