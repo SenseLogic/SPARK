f70c95a3a4bb2e073817175fbe817ef37ad63a62
senselogic <dev.senselogic@gmail.com>
2022-12-14 21:55:54 +0100

    % media

--- PROJECT/MEDIA/make_folder_thumbnails.bat
+++ PROJECT/MEDIA/make_folder_thumbnails.bat
@@ -1,6 +1,6 @@
-if not exist "..\www\static\video\%1" mkdir "..\www\static\video\%1"
+if not exist "..\www\static\image\%1" mkdir "..\www\static\image\%1"
 for %%f in (static\video\%1\*.mp4) do (
     ..\..\..\..\TOOL\FFMPEG\bin\ffmpeg -i "%%f" -y -ss 00:00:00 -t 00:00:10 -vf "scale=320:180,fps=12,palettegen" "palette.png"
-    ..\..\..\..\TOOL\FFMPEG\bin\ffmpeg -i "%%f" -y -i "palette.png" -f gif -ss 00:00:00 -t 00:00:10 -lavfi "scale=320:180,fps=12,paletteuse=dither=sierra2" -loop 0 "..\www\static\video\%1\%%~nf_video_thumbnail.gif"
+    ..\..\..\..\TOOL\FFMPEG\bin\ffmpeg -i "%%f" -y -i "palette.png" -f gif -ss 00:00:00 -t 00:00:10 -lavfi "scale=320:180,fps=12,paletteuse=dither=sierra2" -loop 0 "..\www\static\image\%1\%%~nf_video_thumbnail.gif"
     )
 del /q palette.png
