7bfc2b3ac70b8ae89552dff4d12ae210d86fa3a0
senselogic <dev.senselogic@gmail.com>
2025-01-20 20:15:05 +0100

    % media

--- PROJECT/MEDIA/make_folder_video_thumbnails.bat
+++ PROJECT/MEDIA/make_folder_video_thumbnails.bat
@@ -1,5 +1,5 @@
 if not exist "..\www\static\image\%1" mkdir "..\www\static\image\%1"
 for %%f in (static\video\%1\*.mp4) do (
-    %TOOL%\REMIX\remix %2 %TOOL%\FFMPEG\bin\ffmpeg -i "%%f" -y -ss 00:00:00 -t 00:00:10 -vf "scale=320:180,fps=12" -loop 0 -an -vcodec libwebp -lossless 0 -q:v 50 -compression_level 6 -preset default -an -vsync 0 "..\www\static\image\%1\%%~nf_video_thumbnail.webp"
-    %TOOL%\REMIX\remix %2 %TOOL%\IMAGE_MAGICK\convert "..\www\static\image\%1\%%~nf_video_thumbnail.webp[0]" -resize 360x720 -quality 30 -filter Lanczos -strip "..\www\static\image\%1\%%~nf_video_thumbnail.webp.preload.avif"
+    %TOOL%\REMIX\remix %2 %TOOL%\FFMPEG\bin\ffmpeg -i "%%f" -y -ss 00:00:00 -t 00:00:10 -loop 0 -an -vf "scale=320:180,fps=12" -c:v libaom-av1 -pix_fmt yuv420p -crf 30 -b:v 0 "..\www\static\image\%1\%%~nf_video_thumbnail.avif"
+    %TOOL%\REMIX\remix %2 %TOOL%\IMAGE_MAGICK\convert "..\www\static\image\%1\%%~nf_video_thumbnail.avif" -resize 360x720 -quality 30 -filter Lanczos -strip "..\www\static\image\%1\%%~nf_video_thumbnail.avif.preload.avif"
 )
--- PROJECT/MEDIA/make_folder_videos.bat
+++ PROJECT/MEDIA/make_folder_videos.bat
@@ -1,6 +1,6 @@
 if not exist "..\www\static\video\%1" mkdir "..\www\static\video\%1"
 for %%f in (static\video\%1\*.mp4) do (
-    %TOOL%\REMIX\remix %2 %TOOL%\FFMPEG\bin\ffmpeg -y -i "%%f" -vf scale=640:-2 -c:v libx264 -crf 30 -preset slow -c:a aac "..\www\static\video\%1\%%~nf.mp4.small.mp4"
-    %TOOL%\REMIX\remix %2 %TOOL%\FFMPEG\bin\ffmpeg -y -i "%%f" -vf scale=1280:-2 -c:v libx264 -crf 30 -preset slow -c:a aac "..\www\static\video\%1\%%~nf.mp4.wide.mp4"
-    %TOOL%\REMIX\remix %2 %TOOL%\FFMPEG\bin\ffmpeg -y -i "%%f" -vf scale=1920:-2 -c:v libx264 -crf 30 -preset slow -c:a aac "..\www\static\video\%1\%%~nf.mp4"
+    %TOOL%\REMIX\remix %2 %TOOL%\FFMPEG\bin\ffmpeg -y -i "%%f" -vf scale=640:-2 -c:v libx264 -crf 30 -preset slow -c:a aac -movflags +faststart "..\www\static\video\%1\%%~nf.mp4.small.mp4"
+    %TOOL%\REMIX\remix %2 %TOOL%\FFMPEG\bin\ffmpeg -y -i "%%f" -vf scale=1280:-2 -c:v libx264 -crf 30 -preset slow -c:a aac -movflags +faststart "..\www\static\video\%1\%%~nf.mp4.wide.mp4"
+    %TOOL%\REMIX\remix %2 %TOOL%\FFMPEG\bin\ffmpeg -y -i "%%f" -vf scale=1920:-2 -c:v libx264 -crf 30 -preset slow -c:a aac -movflags +faststart "..\www\static\video\%1\%%~nf.mp4"
 )
