e23763ef86d48ad30c2721aa15378a8d77cda877
senselogic <dev.senselogic@gmail.com>
2025-01-28 21:09:43 +0100

    % media

--- PROJECT/MEDIA/make_folder_images.bat
+++ PROJECT/MEDIA/make_folder_images.bat
@@ -5,7 +5,7 @@ for %%f in (static\image\%1\*.*) do (
     %TOOL%\REMIX\remix %2 %TOOL%\IMAGE_MAGICK\magick "%%f" -resize "640x1280^>" -quality 70 -filter Lanczos -strip "..\www\static\image\%1\%%~nf.avif.small.avif"
     %TOOL%\REMIX\remix %2 %TOOL%\IMAGE_MAGICK\magick "%%f" -resize "960x1920^>" -quality 70 -filter Lanczos -strip "..\www\static\image\%1\%%~nf.avif.medium.avif"
     %TOOL%\REMIX\remix %2 %TOOL%\IMAGE_MAGICK\magick "%%f" -resize "1280x2560^>" -quality 70 -filter Lanczos -strip "..\www\static\image\%1\%%~nf.avif.wide.avif"
-    %TOOL%\REMIX\remix %2 %TOOL%\IMAGE_MAGICK\magick "%%f" -resize "1920x3840^>" -quality 70 -filter Lanczos -strip "..\www\static\image\%1\%%~nf.avif"
-    %TOOL%\REMIX\remix %2 %TOOL%\IMAGE_MAGICK\magick "%%f" -resize "2560x5120^>" -quality 70 -filter Lanczos -strip "..\www\static\image\%1\%%~nf.avif.big.avif"
-    %TOOL%\REMIX\remix %2 %TOOL%\IMAGE_MAGICK\magick "%%f" -resize "3840x7680^>" -quality 70 -filter Lanczos -strip "..\www\static\image\%1\%%~nf.avif.huge.avif"
+    %TOOL%\REMIX\remix %2 %TOOL%\IMAGE_MAGICK\magick "%%f" -resize "1920x1920^>" -quality 70 -filter Lanczos -strip "..\www\static\image\%1\%%~nf.avif"
+    %TOOL%\REMIX\remix %2 %TOOL%\IMAGE_MAGICK\magick "%%f" -resize "2560x2560^>" -quality 70 -filter Lanczos -strip "..\www\static\image\%1\%%~nf.avif.big.avif"
+    %TOOL%\REMIX\remix %2 %TOOL%\IMAGE_MAGICK\magick "%%f" -resize "3840x3840^>" -quality 70 -filter Lanczos -strip "..\www\static\image\%1\%%~nf.avif.huge.avif"
 )
