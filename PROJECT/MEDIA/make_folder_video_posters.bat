bc7d08c1af2050fd8b02d254487b205dfbfe80bc
SenseLogic <dev.senselogic@gmail.com>
2025-10-26 21:22:19 +0100

    % media

--- PROJECT/MEDIA/make_folder_video_posters.bat
+++ PROJECT/MEDIA/make_folder_video_posters.bat
@@ -1,5 +1,5 @@
 if not exist "..\www\static\image\%1" mkdir "..\www\static\image\%1"
 for %%f in (static\video\%1\*.mp4) do (
     %TOOL%\REMIX\remix %2 %TOOL%\FFMPEG\bin\ffmpeg -y -i "%%f" -frames:v 1 "static\image\%1\%%~nf_video_frame.png"
-    %TOOL%\REMIX\remix %2 %TOOL%\REALESRGAN\realesrgan-ncnn-vulkan -i "static\image\%1\%%~nf_video_frame.png" -s 2 -o "static\image\%1\%%~nf_video_background.png"
+    %TOOL%\REMIX\remix %2 %TOOL%\UPSCAYL\upscayl-bin.exe -n upscayl-standard-4x -i "static\image\%1\%%~nf_video_frame.png" -w 3840 -o "static\image\%1\%%~nf_video_background.png"
 )
