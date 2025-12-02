8bdb66598154a7ae1561b08814d401f68c8c9291
SenseLogic <dev.senselogic@gmail.com>
2025-12-02 21:25:11 +0100

    % script

--- PROJECT/CODE/build.bat
+++ PROJECT/CODE/build.bat
@@ -2,4 +2,4 @@
 copy SCRIPT\*.js ..\www\static\script\
 call stylus STYLE/style.styl -o ../www/static
 call stylus ADMINISTRATION/STYLE/administration_style.styl -o ../www/static
-%TOOL%\RESYNC\resync --updated --changed --removed --added --emptied ../www/ C:/xampp/htdocs/
+%TOOL%\RESYNC\resync --updated --changed --removed --added --emptied ../www/ C:/laragon/www/
