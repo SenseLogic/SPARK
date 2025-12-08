1c4a07b2a9e0053a1416f67b7fadddfba4b807ae
SenseLogic <dev.senselogic@gmail.com>
2025-12-08 21:37:09 +0100

    % media

--- PROJECT/MEDIA/make_folder_video_thumbnails.bat
+++ PROJECT/MEDIA/make_folder_video_thumbnails.bat
@@ -1,5 +1,5 @@
 if not exist "..\www\static\image\%1" mkdir "..\www\static\image\%1"
 for %%f in (static\video\%1\*.mp4) do (
-    %TOOL%\REMIX\remix %2 %TOOL%\FFMPEG\bin\ffmpeg -i "%%f" -y -ss 00:00:00 -t 00:00:10 -loop 0 -an -vf "scale=320:180,fps=12" -c:v libaom-av1 -pix_fmt yuv420p -crf 30 -b:v 0 "..\www\static\image\%1\%%~nf_video_thumbnail.avif"
+    %TOOL%\REMIX\remix %2 %TOOL%\FFMPEG\bin\ffmpeg -i "%%f" -y -ss 00:00:00 -t 00:00:10 -loop 0 -an -vf "scale=320:180,fps=12" -c:v av1_nvenc -pix_fmt yuv420p -crf 30 -b:v 0 "..\www\static\image\%1\%%~nf_video_thumbnail.avif"
     %TOOL%\REMIX\remix %2 %TOOL%\IMAGE_MAGICK\magick "..\www\static\image\%1\%%~nf_video_thumbnail.avif" -resize 360x720 -quality 30 -filter Lanczos -strip "..\www\static\image\%1\%%~nf_video_thumbnail.avif.preload.avif"
 )
